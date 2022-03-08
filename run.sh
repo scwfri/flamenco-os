22fec81d10a2c9524f107a04b4cedf3a529eda19
scwfri <scwfri@gmail.com>
2022-03-08 17:05:46 -0600

    Meta: Init kernel bootable in qemu

---
+++ run.sh
@@ -0,0 +1,1 @@
+qemu-system-x86_64 -drive format=raw,file=target/x86_64-flamenco_os/debug/bootimage-flamenco.bin
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,19 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // entry point.. linker looks for _start as default function
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
