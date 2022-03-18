bf5965394166c3d4735bd79b5b7c84e08428f5f1
scwfri <scwfri@gmail.com>
2022-03-18 17:10:19 -0500

    Vga: Now we can print using write!

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,14 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
 extern crate rlibc;
 
 use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn rust_main() {
-    let hello = b"Hello World!";
-    let color_byte = 0x1f;
-
-    let mut hello_colored = [color_byte; 24];
-    for (i, char_byte) in hello.into_iter().enumerate() {
-        hello_colored[i * 2] = *char_byte;
-    }
-
-    let buffer_ptr = (0xb8000 + 1988) as *mut _;
-    unsafe { *buffer_ptr = hello_colored };
+    vga_buffer::test_print();
 
     loop {}
 }
